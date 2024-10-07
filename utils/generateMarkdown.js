2be551582ae01653cd0de8395a6b8e5620489868
John Ferlito <johnferlito73@gmail.com>
2024-10-06 22:48:46 -0400

    clean the code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license==='MIT'){
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-  } else if(license==='ISC'){
-    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === 'APACHE 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === 'GPL 3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === 'BSD 3') {
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else {
+    return '';
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `For more information on this license, visit: [${license} License](https://opensource.org/licenses/${license.replace(/ /g, '_')})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `## License\nThis project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -30,8 +46,11 @@ function generateMarkdown(data) {
 
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
+  - [Contributing](#contributing)
+  - [Test](#test)
+  - [Questions](questions)
+
 
   ## Installation
 
@@ -41,35 +60,21 @@ function generateMarkdown(data) {
 
   ${data.usage}
 
-  ## Credits
-
-  ${data.credits}
-
   ## License
 
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  ${data.license}
 
-  ---
+  ## Contributing
 
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  ${data.contributing}
 
-  ## Badges
+  ## Test
 
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  ${data.tests}
 
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  ## Questions
 
-  ## Features
-
-  If your project has a lot of features, list them here.
-
-  ## How to Contribute
-
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-  ## Tests
-
-  Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+ If you have any questions about the repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [GitHub](https://github.com/${data.github}).
 `;
 }
 
