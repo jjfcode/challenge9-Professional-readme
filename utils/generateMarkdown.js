bceb2e6d739fd20b3b14c8c85188332bb8f335f5
John Ferlito <johnferlito73@gmail.com>
2024-10-10 18:25:13 -0400

    fix indentation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,81 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'MIT') {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  } else if (license === 'APACHE 2.0') {
-    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-  } else if (license === 'GPL 3.0') {
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-  } else if (license === 'BSD 3') {
-    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
-  } else {
-    return '';
-  }
+    if (license === 'MIT') {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    } else if (license === 'APACHE 2.0') {
+        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    } else if (license === 'GPL 3.0') {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    } else if (license === 'BSD 3') {
+        return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+    } else {
+        return '';
+    }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === 'None') {
-    return '';
-  }
-  return `For more information on this license, visit: [${license} License](https://opensource.org/licenses/${license.replace(/ /g, '_')})`;
+    if (license === 'None') {
+        return '';
+    }
+    return `For more information on this license, visit: [${license} License](https://opensource.org/licenses/${license.replace(/ /g, '_')})`;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === 'None') {
-    return '';
-  }
-  return `## License\nThis project is licensed under the ${license} license.`;
+    if (license === 'None') {
+        return '';
+    }
+    return `## License\nThis project is licensed under the ${license} license.`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
 
-  ${renderLicenseBadge(data.license)}
+    ${renderLicenseBadge(data.license)}
 
-  ## Description
+    ## Description
 
-  ${data.description}
+    ${data.description}
 
-  ## Table of Contents
+    ## Table of Contents
 
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributing)
-  - [Test](#test)
-  - [Questions](#questions)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Test](#test)
+    - [Questions](#questions)
 
 
-  ## Installation
-  \`\`\`
-  ${data.installation}
-  \`\`\`
-  ## Usage
+    ## Installation
+    \`\`\`
+    ${data.installation}
+    \`\`\`
+    ## Usage
 
-  ${data.usage}
+    ${data.usage}
 
-  ## License
+    ## License
 
-  ${data.license}
+    ${data.license}
 
-  ## Contributing
+    ## Contributing
 
-  ${data.contributing}
+    ${data.contributing}
 
-  ## Test
-  \`\`\`
-  ${data.tests}
-  \`\`\`
-  ## Questions
+    ## Test
+    \`\`\`
+    ${data.tests}
+    \`\`\`
+    ## Questions
 
- If you have any questions about the repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [GitHub](https://github.com/${data.github}).
-`;
+    If you have any questions about the repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [GitHub](https://github.com/${data.github}).
+    `;
 }
 
 module.exports = generateMarkdown;
