98df1c342e1e99d7e52bf61a0c5d4de90a4ee27f
John Ferlito <johnferlito73@gmail.com>
2024-10-21 18:00:57 -0400

    fix test

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const inquirer=require('inquirer')
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type:'input',
-        message:'What is your GitHub username?',
-        name:'github',
+        type:'input',   // Input type: text
+        message:'What is your GitHub username?',    // Prompt message
+        name:'github',  // Name for the response
     },
     {
         type:'input',
         message:'What is your email address?',
         name:'email',
-        validate: function(value) {
-            const valid = /\S+@\S+\.\S+/;
-            return valid.test(value) || 'Please enter a valid email address.'
+        validate: function(value) {         // Validation function for email format
+            const valid = /\S+@\S+\.\S+/;   // Regex for validating email
+            return valid.test(value) || 'Please enter a valid email address.' // Return validation result
         },
     },
     {
-        type:'input',
-        message:'What is your project\'s name?',
-        name:'title',
+        type:'input',   // Input type: text
+        message:'What is your project\'s name?',    // Prompt message
+        name:'title',   // Name for the response
     },
     {
         type:'input',
@@ -29,10 +29,10 @@ const questions = [
         name:'description',
     },
     {
-        type:'list',
-        message:'Chose the following licenses:',
-        name:'license',
-        choices:['MIT','APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        type:'list',    // Input type: list selection
+        message:'Chose the following licenses:',    // Prompt message
+        name:'license', // Name for the response
+        choices:['MIT','APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],   // License options
     },
     {
         type:'input',
@@ -42,9 +42,9 @@ const questions = [
     },
     {
         type:'input',
-        message:'What command should be run to run tests?',
-        name:'tests',
-        default:'npm tests',
+        message:'What command should be run to run test?',
+        name:'test',
+        default:'npm test',
     },
     {
         type:'input',
@@ -60,18 +60,18 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const content = generateMarkdown(data)
-    fs.writeFile(fileName, content,(err)=>err?console.error(err):console.log('Generating README...')) 
+    const content = generateMarkdown(data)  // Generate markdown content from user data
+    fs.writeFile(fileName, content,(err)=>err?console.error(err):console.log('Generating README...'))   // Handle file writing and log status
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt(questions)
-    .then(data=>{
-        writeToFile('./output/README.md',data)
+    .prompt(questions)  // Prompt the user with the defined questions
+    .then(data=>{   // Process the user's responses
+        writeToFile('./output/README.md',data)  // Write the responses to README.md file
     })
 }
 
 // Function call to initialize app
-init();
+init(); // Start the application
